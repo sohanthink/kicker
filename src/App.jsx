55e84fb2ad664748e66dc76c15287c32b8aedb3c
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-03-24 03:07:17 +0600

    project set and tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-7xl font-bold underline">
+        Hello world!
+      </h1>
+      <h2 className='text-3xl font-bold text-cyan-500 bg-slate-500 inline-block px-5 py-3 rounded-xl'>tailwind css added</h2>
+    </>
+  )
+}
+
+export default App
