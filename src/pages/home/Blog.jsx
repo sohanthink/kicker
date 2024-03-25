0d8bf3e2d21beab4a8836d11d028aceaa70cce68
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-03-25 16:29:53 +0600

    responsive

--- src/pages/home/Blog.jsx
+++ src/pages/home/Blog.jsx
@@ -4,15 +4,17 @@ import TopBlogCard from './TopBlogCard'
 const Blog = () => {
     return (
         <div className='w-11/12 md:w-4/5 mx-auto grid grid-cols-12'>
-            <div className='col-span-8'>
+            <div className='sm:col-span-8 col-span-12'>
                 <TopBlogCard img='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=800' content="Changing your vision of many things is hard, but possible. Try these…" stitle='branding' title='Being a Brand Expert' />
 
-                <TopBlogCard img='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=800' content="Changing your vision of many things is hard, but possible. Try these…" stitle='branding' title='Being a Brand Expert' />
 
-                <TopBlogCard img='https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&w=800' content="Changing your vision of many things is hard, but possible. Try these…" stitle='branding' title='Being a Brand Expert' />
+                <TopBlogCard img='https://images.pexels.com/photos/1311587/pexels-photo-1311587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' content="We have gathered several projects that will catch your interest as a…" stitle='DESIGN' title='Trending Architecture Styles' />
+
+
+                <TopBlogCard img='https://images.pexels.com/photos/2228183/pexels-photo-2228183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' content="Every now and then a designer needs help getting his projects organized" stitle='DEv-P' title='Top Modern Apps and Gadgets' />
 
             </div>
-            <div className='col-span-4'>nice</div>
+            <div className='sm:col-span-4 hidden sm:block'>nice</div>
         </div>
     )
 }
--- src/pages/home/TopBlogCard.jsx
+++ src/pages/home/TopBlogCard.jsx
@@ -9,7 +9,7 @@ const TopBlogCard = ({ img, stitle, title, content }) => {
     let [icon, setIcon] = useState(false)
     let [card, setCard] = useState(false)
 
-
+    // detect icon hover nad leave
     let handleiconover = () => {
         setIcon(true)
     }
@@ -17,6 +17,7 @@ const TopBlogCard = ({ img, stitle, title, content }) => {
         setIcon(false)
     }
 
+    // detect card hover and leave hover
     let handleCardOver = () => {
         setCard(true)
     }
@@ -27,16 +28,16 @@ const TopBlogCard = ({ img, stitle, title, content }) => {
 
     return (
         <>
-            <div onMouseOver={handleCardOver} onMouseLeave={handleCardLeave} className='flex gap-8'>
-                <div className='w-1/2 overflow-hidden'>
+            <div onMouseOver={handleCardOver} onMouseLeave={handleCardLeave} className='flex gap-4 sm:gap-8 flex-col sm:flex-row my-5'>
+                <div className='w-full sm:w-2/5 overflow-hidden'>
                     <Image className={`w-full h-full object-cover transition ease-in-out duration-500 ${card && "scale-125"}`} source={img} />
                 </div>
-                <div className='flex flex-col gap-3 justify-center'>
+                <div className='flex flex-col gap-3 justify-center w-fulll sm:w-3/5'>
                     <h6 className='font-primary uppercase text-slate-500'>{stitle}</h6>
 
-                    <h3 className={`font-primary text-slate-800 text-3xl font-medium transition-all after:transition-all after:duration-500 relative after:absolute after:w-0 after:h-1 after:top-9 after:left-0 after:content-[''] after:bg-slate-500 ${card && "after:w-3/4"}`}>{title}</h3>
+                    <h3 className={`font-primary text-slate-800 text-2xl sm:text-3xl font-medium transition-all after:transition-all after:duration-500 relative after:absolute after:w-0 after:h-1 after:top-7 sm:after:top-9 after:left-0 after:content-[''] after:bg-slate-500 ${card && "after:w-3/4"}`}>{title}</h3>
 
-                    <span className='uppercase font-semibold text-sm'><strong>By sandra jones</strong> 19 feb 2029 22:11</span>
+                    <span className='uppercase font-semibold text-xs sm:text-sm'><strong>By sandra jones</strong> 19 feb 2029 22:11</span>
 
                     <p className='font-primary text-gray-400'>{content}</p>
 
