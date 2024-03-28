87aa5fe0b161107f51eee3a8f2197c71e9fca6a7
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-03-28 15:15:27 +0600

    fashion category onf0ing

--- src/pages/categories/fashion/Fashion.jsx
+++ src/pages/categories/fashion/Fashion.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryBanner from '../../../components/CategoryBanner'
 import CategoryBlog from '../CategoryBlog'
 import Ads from '../../../components/Ads'
+import Image from '../../../utilities/Image'
 
 const Fashion = () => {
     return (
@@ -11,6 +12,21 @@ const Fashion = () => {
                 category_heading="Trending Colors for Upcoming Spring"
                 source="https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
+
+            <div className='w-11/12 md:w-11/12 mx-auto font-primary my-10 space-y-6'>
+                <div className='w-3/4 mx-auto space-y-4'>
+                    <h6 className='uppercase text-red-600 font-medium'>Fashion</h6>
+                    <h2 className='text-6xl font-bold text-gray-950/90'>The Cutest Fashion Trends of the Decade.</h2>
+                    <p className='text-gray-950/90 font-light'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quis voluptates soluta rerum, praesentium sequi eius ad officia accusantium facere! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quis voluptates soluta rerum, praesentium sequi eius ad officia accusantium facere!</p>
+
+                    <p className='text-gray-950/90 font-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit deserunt eveniet esse commodi perspiciatis ipsum. Delectus aliquid cumque obcaecati minus doloribus eum nulla sit molestiae amet porro facere omnis, dolore quo vitae, corporis tempora aspernatur! Repellat, officia, asperiores magni ex quam est ad perspiciatis, eos nostrum rem aliquid distinctio facere?</p>
+                </div>
+
+                <Image source="https://images.pexels.com/photos/886404/pexels-photo-886404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+            </div>
+
+
+            {/* scroll part starts here */}
             <div className='w-11/12 md:w-4/5 mx-auto grid grid-cols-12 gap-x-4 font-primary relative items-start'>
 
                 <div className='col-span-12 md:col-span-8 '>
@@ -25,7 +41,8 @@ const Fashion = () => {
                     <div className='border-b-2 py-9'>
                         <CategoryBlog
                             source="https://images.pexels.com/photos/833052/pexels-photo-833052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                            subcat="Mans" title="What Designers Can’t Solve"
+                            subcat="Mans"
+                            title="What Designers Can’t Solve"
                             link="https://www.sohanthink.com"
                             description="Qed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium loremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Middle Post…" />
                     </div>
@@ -44,6 +61,7 @@ const Fashion = () => {
                         <Ads />
                     </div>
                 </div>
+
             </div>
 
 
