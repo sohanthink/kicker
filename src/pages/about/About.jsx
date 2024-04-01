55312daadb27da8bdc40b9c74e7af97f31e1ed4d
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-04-02 04:01:24 +0600

    responsive about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -9,10 +9,10 @@ const About = () => {
     return (
         <>
             <div className='w-11/12 mx-auto'>
-                <Image className="w-full h-screen" source="https://images.pexels.com/photos/2127041/pexels-photo-2127041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+                <Image className="w-full md:h-screen" source="https://images.pexels.com/photos/2127041/pexels-photo-2127041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
             </div>
 
-            <div className='mx-auto space-y-4 py-14 font-primary w-2/4'>
+            <div className='mx-auto space-y-4 py-14 font-primary w-11/12 md:w-2/4'>
                 <h3 className='font-medium text-3xl'>Hello! Here we are.</h3>
                 <p className='text-gray-500 text-xl font-thin'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                     Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
@@ -26,11 +26,11 @@ const About = () => {
                 </div>
             </div>
 
-            <div className='mx-auto space-y-4 py-14 font-primary w-2/4'>
+            <div className='mx-auto space-y-4 py-14 font-primary w-11/12 md:w-2/4'>
                 <p className='text-gray-500 text-xl font-thin'>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
             </div>
 
-            <div className='bg-purple-100 w-11/12 mx-auto flex justify-between px-36 py-14 font-primary'>
+            <div className='bg-purple-100 w-11/12 mx-auto flex flex-col md:justify-between md:flex-row px-28 md:px-36 py-14 font-primary gap-5'>
 
                 <div className='flex flex-col items-center space-y-2'>
                     <span className='text-7xl'>789</span>
